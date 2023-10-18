b17df0a58445e0d26149648587ba00261eeaca6d
Et010101 <148325594+Et010101@users.noreply.github.com>
2023-10-18 22:07:25 +0000

    newupdate

--- app.py
+++ app.py
@@ -1,11 +1,36 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
+GROUPS = [{
+    'id': 1,
+    'name': 'Popular Academic Subjects',
+    's1': 'Science',
+    's2': 'Computer cience',
+    's3': 'Engineering'
+}, {
+    'id': 2,
+    'name': 'Hobbies & Interests',
+    's1': 'Creative Writing',
+    's2': 'Cooking',
+    's3': 'Fitness & Wellness'
+},
+ {
+   'id': 3,
+ 'name': 'Support & Networking',
+ 's1': 'Mental Health Support',
+ 's2': 'LGBTQ Support',
+ 's3': 'Women In Area'
+ },
+{ 'id': 4,
+  'name': 'Miscellaneous',
+  's1': 'Books Clubs',
+  's2': 'Study Techniques',
+  's3': 'Research Groups'}]
 
 
 @app.route("/")
 def hello_world():
-  return render_template('home.html')
+  return render_template('home.html', groups=GROUPS)
 
 
 if __name__ == "__main__":
